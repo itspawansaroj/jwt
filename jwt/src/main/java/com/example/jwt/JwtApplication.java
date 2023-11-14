906f51b350106105e9b8cf13f2f29e5853032a91
itspawansaroj <149998714+itspawansaroj@users.noreply.github.com>
2023-11-15 00:10:00 +0530

    Update JwtApplication.java

--- jwt/src/main/java/com/example/jwt/JwtApplication.java
+++ jwt/src/main/java/com/example/jwt/JwtApplication.java
@@ -30,6 +30,141 @@ public class JwtApplication {
 		}  
 		System.out.println(charCount); 
 		System.out.println(charCount); 
+		HashMap <Character, Integer> charCount1 = new HashMap<>();  
+		for (int i = str.length() - 1; i >= 0; i--)   
+		{  
+		if(charCount1.containsKey(str.charAt(i)))   
+		{  
+		int count = charCount1.get(str.charAt(i));  
+		charCount1.put(str.charAt(i), ++count);  
+		}   
+		else   
+		{  
+			charCount1.put(str.charAt(i),1);  
+		}  
+		}  
+		System.out.println(charCount1); 
+		System.out.println(charCount1); 
+		HashMap <Character, Integer> charCount2 = new HashMap<>();  
+		for (int i = str.length() - 1; i >= 0; i--)   
+		{  
+		if(charCount2.containsKey(str.charAt(i)))   
+		{  
+		int count = charCount2.get(str.charAt(i));  
+		charCount2.put(str.charAt(i), ++count);  
+		}   
+		else   
+		{  
+			charCount2.put(str.charAt(i),1);  
+		}  
+		}  
+		System.out.println(charCount2); 
+		System.out.println(charCount2); 
+		HashMap <Character, Integer> charCount4 = new HashMap<>();  
+		for (int i = str.length() - 1; i >= 0; i--)   
+		{  
+		if(charCount4.containsKey(str.charAt(i)))   
+		{  
+		int count = charCount4.get(str.charAt(i));  
+		charCount4.put(str.charAt(i), ++count);  
+		}   
+		else   
+		{  
+			charCount4.put(str.charAt(i),1);  
+		}  
+		}  
+		System.out.println(charCount4); 
+		System.out.println(charCount4); 
+		HashMap <Character, Integer> charCount3 = new HashMap<>();  
+		for (int i = str.length() - 1; i >= 0; i--)   
+		{  
+		if(charCount3.containsKey(str.charAt(i)))   
+		{  
+		int count = charCount3.get(str.charAt(i));  
+		charCount3.put(str.charAt(i), ++count);  
+		}   
+		else   
+		{  
+			charCount3.put(str.charAt(i),1);  
+		}  
+		}  
+		System.out.println(charCount3); 
+		System.out.println(charCount3); 
+		HashMap <Character, Integer> charCount7 = new HashMap<>();  
+		for (int i = str.length() - 1; i >= 0; i--)   
+		{  
+		if(charCount.containsKey(str.charAt(i)))   
+		{  
+		int count = charCount.get(str.charAt(i));  
+		charCount.put(str.charAt(i), ++count);  
+		}   
+		else   
+		{  
+		charCount.put(str.charAt(i),1);  
+		}  
+		}  
+		System.out.println(charCount); 
+		System.out.println(charCount); 
+		HashMap <Character, Integer> charCount5 = new HashMap<>();  
+		for (int i = str.length() - 1; i >= 0; i--)   
+		{  
+		if(charCount.containsKey(str.charAt(i)))   
+		{  
+		int count = charCount.get(str.charAt(i));  
+		charCount.put(str.charAt(i), ++count);  
+		}   
+		else   
+		{  
+		charCount.put(str.charAt(i),1);  
+		}  
+		}  
+		System.out.println(charCount); 
+		System.out.println(charCount); 
+		HashMap <Character, Integer> charCount6 = new HashMap<>();  
+		for (int i = str.length() - 1; i >= 0; i--)   
+		{  
+		if(charCount.containsKey(str.charAt(i)))   
+		{  
+		int count = charCount.get(str.charAt(i));  
+		charCount.put(str.charAt(i), ++count);  
+		}   
+		else   
+		{  
+		charCount.put(str.charAt(i),1);  
+		}  
+		}  
+		System.out.println(charCount); 
+		System.out.println(charCount); 
+		HashMap <Character, Integer> charCount8 = new HashMap<>();  
+		for (int i = str.length() - 1; i >= 0; i--)   
+		{  
+		if(charCount.containsKey(str.charAt(i)))   
+		{  
+		int count = charCount.get(str.charAt(i));  
+		charCount.put(str.charAt(i), ++count);  
+		}   
+		else   
+		{  
+		charCount.put(str.charAt(i),1);  
+		}  
+		}  
+		System.out.println(charCount); 
+		System.out.println(charCount); 
+		HashMap <Character, Integer> charCount9 = new HashMap<>();  
+		for (int i = str.length() - 1; i >= 0; i--)   
+		{  
+		if(charCount.containsKey(str.charAt(i)))   
+		{  
+		int count = charCount.get(str.charAt(i));  
+		charCount.put(str.charAt(i), ++count);  
+		}   
+		else   
+		{  
+		charCount.put(str.charAt(i),1);  
+		}  
+		}  
+		System.out.println(charCount); 
+		System.out.println(charCount); 
 		}  
 	}
 

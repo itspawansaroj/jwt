197e13c6c85a3eb0ef62a4da6d863a7a6ad67f80
itspawansaroj <149998714+itspawansaroj@users.noreply.github.com>
2023-11-14 23:51:42 +0530

    Update JwtApplication.java

--- jwt/src/main/java/com/example/jwt/JwtApplication.java
+++ jwt/src/main/java/com/example/jwt/JwtApplication.java
@@ -1,5 +1,7 @@
 package com.example.jwt;
 
+import java.util.HashMap;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,6 +11,25 @@ public class JwtApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(JwtApplication.class, args);
 		System.out.println("Welcome");
+		System.out.println("test");
+		
+		String str = "google";  
+		//HashMap char as a key and occurrence as a value  
+		HashMap <Character, Integer> charCount = new HashMap<>();  
+		for (int i = str.length() - 1; i >= 0; i--)   
+		{  
+		if(charCount.containsKey(str.charAt(i)))   
+		{  
+		int count = charCount.get(str.charAt(i));  
+		charCount.put(str.charAt(i), ++count);  
+		}   
+		else   
+		{  
+		charCount.put(str.charAt(i),1);  
+		}  
+		}  
+		System.out.println(charCount); 
+		System.out.println(charCount); 
+		}  
 	}
 
-}
